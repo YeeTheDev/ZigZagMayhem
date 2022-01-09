c31c152deb023d1a0d69e95b75135d57484e4000
Yee <yee.gamedev@gmail.com>
2022-01-08 21:12:59 -0600

    Turn Head With Mouse Position

---
+++ Assets/Scripts/TankController.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TankController : MonoBehaviour
+{
+    [SerializeField] Transform tankHead = null;
+
+    Camera cam;
+
+    private void Start()
+    {
+        cam = Camera.main;
+    }
+
+    public void Update()
+    {
+        Vector3 point = cam.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y,
+            transform.position.z - cam.transform.position.z));
+        point.y = 0;
+
+        tankHead.rotation = Quaternion.LookRotation(point, Vector3.up);
+    }
+
+}
