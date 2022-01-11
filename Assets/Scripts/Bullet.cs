4784a7dfcb8ee64396a6a83366b11d592f01fcea
Yee <yee.gamedev@gmail.com>
2022-01-11 17:42:48 -0600

    Bullet Script Created

---
+++ Assets/Scripts/Bullet.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+    [SerializeField] float timeToReuse = 4;
+
+    int timesEnqueue;
+    ObjectPooler pooler;
+
+    public ObjectPooler SetPooler { set => pooler = value; }
+
+    private void OnEnable()
+    {
+        StartCoroutine(DisableBullet());
+    }
+
+    private IEnumerator DisableBullet()
+    {
+        timesEnqueue++;
+        if (timesEnqueue <= 1) { yield break; }
+
+        yield return new WaitForSeconds(timeToReuse);
+
+        gameObject.SetActive(false);
+        pooler.EnqueueObject(gameObject);
+    }
+}
