6595960a1c96e4ed728de4da14ec4eda4144ad01
Yee <yee.gamedev@gmail.com>
2022-01-11 11:37:52 -0600

    Player Moves Forward

--- Assets/Scripts/TankController.cs
+++ Assets/Scripts/TankController.cs
@@ -2,24 +2,33 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(Rigidbody))]
 public class TankController : MonoBehaviour
 {
     [SerializeField] Transform tankHead = null;
+    [Range(-10,10)][SerializeField] float mousePrecision = 4;
+    [SerializeField] float moveSpeed = 2;
 
-    Camera cam;
+    Rigidbody rb;
 
-    private void Start()
+    private void Awake()
     {
-        cam = Camera.main;
+        rb = GetComponent<Rigidbody>();
     }
 
     public void Update()
     {
-        Vector3 point = cam.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y,
-            transform.position.z - cam.transform.position.z));
-        point.y = 0;
+        RotateCannonToMouseCursor();
 
-        tankHead.rotation = Quaternion.LookRotation(point, Vector3.up);
+        rb.MovePosition(transform.position + transform.forward * moveSpeed * Time.deltaTime);
     }
 
+    private void RotateCannonToMouseCursor()
+    {
+        Vector3 mouseCursorPoint = Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y,
+            transform.position.z - Camera.main.transform.position.z + mousePrecision));
+        mouseCursorPoint.y = tankHead.position.y;
+
+        tankHead.LookAt(mouseCursorPoint, Vector3.up);
+    }
 }
