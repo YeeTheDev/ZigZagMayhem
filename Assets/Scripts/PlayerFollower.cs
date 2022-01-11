18722ced2ed80804b07296e488331d0180b1117c
Yee <yee.gamedev@gmail.com>
2022-01-11 11:38:30 -0600

    Camera Follows Player

---
+++ Assets/Scripts/PlayerFollower.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerFollower : MonoBehaviour
+{
+    Vector3 vectorOffset;
+    Transform player;
+
+    private void Awake()
+    {
+        player = GameObject.FindWithTag("Player").transform;
+        vectorOffset = player.position - transform.position;
+    }
+
+    private void LateUpdate()
+    {
+        Vector3 followPosition = player.position - vectorOffset;
+
+        transform.position = followPosition;
+    }
+}
