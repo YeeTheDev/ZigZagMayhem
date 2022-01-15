fa4b3272d6499132b6922ce811ac9299759fd8af
Yee <yee.gamedev@gmail.com>
2022-01-15 10:20:35 -0600

    Enemy Get Destroyed When Colliding With Bullet

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -4,15 +4,11 @@ using UnityEngine;
 
 public class Enemy : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    private void OnTriggerEnter(Collider other)
     {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        if (other.CompareTag("Bullet"))
+        {
+            gameObject.SetActive(false);
+        }
     }
 }
