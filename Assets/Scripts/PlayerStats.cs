4fb216d30d2cfe8469d3bef655df1294938733f9
Yee <yee.gamedev@gmail.com>
2022-01-17 10:42:39 -0600

    Player Stats Script Created

---
+++ Assets/Scripts/PlayerStats.cs
@@ -0,0 +1,19 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerStats : MonoBehaviour
+{
+    [SerializeField] int maxHealth = 3;
+
+    public event Action onHealthChange;
+
+    public int Health { get => maxHealth; }
+
+    public void UpdateHealth(int modifier)
+    {
+        maxHealth = Mathf.Clamp(maxHealth + modifier, 0, maxHealth);
+        if (onHealthChange != null) { onHealthChange(); }
+    }
+}
