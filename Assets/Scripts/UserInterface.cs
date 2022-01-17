ea64554d22630d8e9dbd93898938099e2d12dbb0
Yee <yee.gamedev@gmail.com>
2022-01-17 10:50:28 -0600

    User Interface Updates When Player Takes Damage

--- Assets/Scripts/UserInterface.cs
+++ Assets/Scripts/UserInterface.cs
@@ -7,16 +7,21 @@ public class UserInterface : MonoBehaviour
 {
     [SerializeField] RectTransform healthBar = null;
 
+    float sizePerHeart;
     PlayerStats playerStats;
 
     private void Awake()
     {
         playerStats = GameObject.FindGameObjectWithTag("Player").GetComponent<PlayerStats>();
         playerStats.onHealthChange += UpdateHealthBar;
+
+        sizePerHeart = healthBar.sizeDelta.x / playerStats.Health;
     }
 
     private void UpdateHealthBar()
     {
-        
+        Vector2 updatedSize = healthBar.sizeDelta;
+        updatedSize.x = sizePerHeart * playerStats.Health;
+        healthBar.sizeDelta = updatedSize;
     }
 }
