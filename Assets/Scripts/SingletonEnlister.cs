5440704d8bd12da79bb48b05da00a00bc210186f
Yee <yee.gamedev@gmail.com>
2022-01-17 13:45:26 -0600

    Singleton Enlister Script Created

---
+++ Assets/Scripts/SingletonEnlister.cs
@@ -0,0 +1,20 @@
+using System.Collections.Generic;
+
+public sealed class SingletonEnlister
+{
+    private static SingletonEnlister instance;
+    private List<string> singletons = new List<string>();
+
+    public static SingletonEnlister Enlister
+    {
+        get
+        {
+            if (instance == null) { instance = new SingletonEnlister(); }
+            return instance;
+        }
+    }
+
+    public void AddSingleton(string name) { singletons.Add(name); }
+    public void RemoveSingleton(string name) { singletons.RemoveAt(singletons.IndexOf(name)); }
+    public bool AlreadyASingleton(string nameToCheck) { return singletons.Contains(nameToCheck); }
+}
