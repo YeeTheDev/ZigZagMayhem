167883561919f829c19230fd9e8123e1509b357d
Yee <yee.gamedev@gmail.com>
2022-01-17 13:45:40 -0600

    SingletonCreator Script Created

---
+++ Assets/Scripts/SingletonCreator.cs
@@ -0,0 +1,15 @@
+using UnityEngine;
+
+public class SingletonCreator : MonoBehaviour
+{
+    private void Awake()
+    {
+        if (!SingletonEnlister.Enlister.AlreadyASingleton(gameObject.name))
+        {
+            SingletonEnlister.Enlister.AddSingleton(gameObject.name);
+            DontDestroyOnLoad(gameObject);
+        }
+        else { Destroy(gameObject); }
+        
+    }
+}
