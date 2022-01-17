0aab05040e114eab25e8b47398007f8854999a7a
Yee <yee.gamedev@gmail.com>
2022-01-17 13:05:54 -0600

    Rotator Script Created

---
+++ Assets/Scripts/Rotator.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotator : MonoBehaviour
+{
+    [SerializeField] float rotationSpeed = 0;
+
+    private void Update()
+    {
+        transform.Rotate(Vector3.up * rotationSpeed * Time.deltaTime);
+    }
+}
