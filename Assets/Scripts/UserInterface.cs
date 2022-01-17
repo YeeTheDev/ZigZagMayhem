e11f0a310994608e277b8e914c7dc43eb061a759
Yee <yee.gamedev@gmail.com>
2022-01-17 10:42:47 -0600

    UI Script Created

---
+++ Assets/Scripts/UserInterface.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UserInterface : MonoBehaviour
+{
+    [SerializeField] RectTransform healthBar = null;
+
+    PlayerStats playerStats;
+
+    private void Awake()
+    {
+        playerStats = GameObject.FindGameObjectWithTag("Player").GetComponent<PlayerStats>();
+        playerStats.onHealthChange += UpdateHealthBar;
+    }
+
+    private void UpdateHealthBar()
+    {
+        
+    }
+}
