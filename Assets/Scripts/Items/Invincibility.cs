7c5adb787d83edaac9ddb59c8d4730068d36a7b5
Yee <yee.gamedev@gmail.com>
2022-01-18 11:47:02 -0600

    Invincibility Item Created

---
+++ Assets/Scripts/Items/Invincibility.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Invincibility : MonoBehaviour, IITem
+{
+    public IEnumerator UseItem(Transform t = null)
+    {
+        Debug.Log("You are invincible!");
+        yield break;
+    }
+}
