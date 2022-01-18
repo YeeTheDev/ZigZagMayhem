c1d15924aeff6b6abc20b63b3e1e88a0e2d61340
Yee <yee.gamedev@gmail.com>
2022-01-18 11:47:45 -0600

    TankCollisioner Handles Using Items (Noooooo!!!)

---
+++ Assets/Scripts/TankCollisioner.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TankCollisioner : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Item"))
+        {
+            StartCoroutine(ActivateItem(other.GetComponent<ItemBuilder>().ItemSequence));
+        }
+    }
+
+    private IEnumerator ActivateItem(List<IITem> itemSequence)
+    {
+        foreach (IITem item in itemSequence)
+        {
+            yield return item.UseItem(transform);
+        }
+
+        Debug.Log("Finished Using Item");
+    }
+}
