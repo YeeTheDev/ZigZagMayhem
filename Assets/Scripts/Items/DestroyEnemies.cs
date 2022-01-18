9ba079e12e14b7db3256c74c02a02936b3a91953
Yee <yee.gamedev@gmail.com>
2022-01-18 11:47:13 -0600

    DestroyEnemies Item Created

---
+++ Assets/Scripts/Items/DestroyEnemies.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyEnemies : MonoBehaviour, IITem
+{
+    public IEnumerator UseItem(Transform t = null)
+    {
+        foreach (GameObject enemy in GameObject.FindGameObjectsWithTag("Enemy"))
+        {
+            enemy.SetActive(false);
+        }
+
+        yield break;
+    }
+}
