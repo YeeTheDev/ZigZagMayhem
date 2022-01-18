6249c0a3c3643d252cbb9cbe3c1b7ff4d610ba4c
Yee <yee.gamedev@gmail.com>
2022-01-18 11:47:27 -0600

    ItemBuilder Created

---
+++ Assets/Scripts/Items/ItemBuilder.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemBuilder : MonoBehaviour
+{
+    [SerializeField] MonoBehaviour[] abilities = null;
+
+    List<IITem> itemSequence = new List<IITem>();
+
+    public List<IITem> ItemSequence { get => itemSequence; }
+
+    private void Awake() { CreateItemSequence(); }
+
+    private void CreateItemSequence()
+    {
+        foreach (MonoBehaviour itemToCheck in abilities)
+        {
+            IITem item = (IITem)itemToCheck;
+
+            if (item != null)
+            {
+                itemSequence.Add(item);
+            }
+        }
+
+        foreach (IITem item in itemSequence)
+        {
+            Debug.Log(item.ToString());
+        }
+    }
+}
