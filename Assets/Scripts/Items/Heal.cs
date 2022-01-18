b9f94508061fe0c394f65936be4f5ce24356e164
Yee <yee.gamedev@gmail.com>
2022-01-18 11:46:52 -0600

    Item Heal Created

---
+++ Assets/Scripts/Items/Heal.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Heal : MonoBehaviour, IITem
+{
+    public IEnumerator UseItem(Transform player)
+    {
+        player.GetComponent<PlayerStats>().UpdateHealth(1);
+        yield break;
+    }
+}
