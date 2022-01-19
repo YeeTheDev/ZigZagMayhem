1bf3a34bcef494caad65bd712f90fd223b67718d
Yee <yee.gamedev@gmail.com>
2022-01-19 16:20:32 -0600

    DelayDecorator Created

---
+++ Assets/Scripts/Items/DelayDecorator.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DelayDecorator : MonoBehaviour, IITem
+{
+    [SerializeField] float delayTime;
+    [SerializeField] MonoBehaviour itemAbility = null;
+
+    public IEnumerator UseItem(Transform t = null)
+    {
+        yield return new WaitForSeconds(delayTime);
+
+        IITem item = itemAbility as IITem;
+
+        if (item != null) { yield return item.UseItem(t); }
+    }
+}
