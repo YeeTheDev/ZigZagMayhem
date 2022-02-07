19565facaede897bbe29f01d745e285cc22ed3ea
Yee <yee.gamedev@gmail.com>
2022-02-07 17:14:02 -0600

    Tutorial Screen Added

--- Assets/Scripts/SceneLoader.cs
+++ Assets/Scripts/SceneLoader.cs
@@ -3,5 +3,5 @@ using UnityEngine.SceneManagement;
 
 public class SceneLoader : MonoBehaviour
 {
-    public void LoadGame() { SceneManager.LoadScene(1); }
+    public void LoadGame(int sceneToLoad) { SceneManager.LoadScene(sceneToLoad); }
 }
---
+++ Assets/Scripts/TutorialScreen.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TutorialScreen : MonoBehaviour
+{
+    SceneLoader sceneLoader;
+
+    private void Awake()
+    {
+        sceneLoader = GetComponent<SceneLoader>();
+    }
+
+    void Update()
+    {
+        if (Input.anyKeyDown) { sceneLoader.LoadGame(2); }
+    }
+}
